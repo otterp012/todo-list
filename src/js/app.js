58702cbd8d4a0fe0e510e20d37dd402d9ac55bf4
otterp012 <otterp012@gmail.com>
2022-04-07 15:27:35 +0900

    feat: todolist 이벤트 등록 진행중

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,83 @@
-import {
-  remove,
-  add,
-  addTodo,
-  cards,
-  column,
-  removeColumn,
-  modalReveal,
-  toDoInputReveal,
-} from "./component.js";
+const store = {
+  focusedCard: null,
+};
 
-const plan = document.querySelector(".todo-plan");
-const done = document.querySelector(".todo-done");
-const columnRemoveBtn = document.querySelectorAll(".column-remove-btn");
-let cardAddBtn = document.querySelector(".column-add-btn");
-let cardRemoveBtn = document.querySelectorAll(".card-remove-btn");
-const modalCancelBtn = document.querySelector(".modal-cancel-btn");
+const setStateProperty = (property, state) => {
+  store[property] = state;
+};
 
-console.log("hi!");
-remove(cardRemoveBtn);
-removeColumn(columnRemoveBtn);
-add(toDoInputReveal());
+const cardRemoveBtnEl = document.querySelector('.card-remove-btn');
+const modalWrapperEl = document.querySelector('.modal-wrapper');
 
-cardAddBtn.addEventListener("click", add);
-modalCancelBtn.addEventListener("click", modalReveal);
+// cardRemoveBtnEl.addEventListener('click', (e) => {
+//   modalWrapperEl.classList.add('active');
+//   console.log(e.currentTarget);
+//   e.currentTarget.setAttribute('focusedCard', true);
+// });
+
+const modalCancelBtnEl = document.querySelector('.modal-cancel-btn');
+modalCancelBtnEl.addEventListener('click', () => {
+  modalWrapperEl.classList.remove('active');
+});
+
+const newCardWrapper = (id) => {
+  return `<div class="new-card-wrapper" id="${id}">
+  <input type="text" class="card-tittle-input" placeholder="제목을 입력하세요">
+  <input type="text" class="card-text-input" placeholder="내용을 입력하세요">
+  <div class="card-btn-wrapper">
+    <button class="card-cancel-btn">취소</button>
+    <button class="card-add-btn">등록</button>
+  </div>
+</div>`;
+};
+
+const cardWrapper = (array) => {
+  const [tittle, text] = array;
+  return `<div class="card-wrapper">
+  <div class="card-header-wrapper">
+    <h3 class="card-tittle">${tittle}</h3>
+    <button class="card-remove-btn"></button>
+  </div>
+  <div class="card-text-wrapper">
+    <p class="card-text">${text}</p>
+  </div>
+  <div class="card-footer-wrapper">
+    <p class="card-footer-text">otter</p>
+  </div>
+</div>`;
+};
+const columnWrapperEl = document.querySelector('.column-wrapper');
+columnWrapperEl.addEventListener('click', (e) => {
+  if (e.target.className === 'column-add-btn') {
+    columnWrapperEl.innerHTML += newCardWrapper('focused');
+  }
+});
+
+// columnWrapperEl.addEventListener('click', (e) => {
+//   if (e.target.className === 'card-cancel-btn') {
+//     document.querySelector('#focused').remove();
+//   }
+// });
+
+const cardWrapperEl = document.querySelector('.card-wrapper');
+
+cardWrapperEl.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (e.target.className === 'card-add-btn') {
+    const focusedCard = document.querySelector('#focused');
+    const data = [...focusedCard.children]
+      .filter((v) => v.tagName === 'INPUT')
+      .map((v) => v.value);
+    columnWrapperEl.innerHTML += cardWrapper(data);
+  }
+
+  if (e.target.className === 'card-cancel-btn') {
+    e.currentTarget.remove();
+  }
+});
+
+// const deleteNode = (query) => document.querySelector(`${query}`).remove();
+
+function deleteNode(query) {
+  document.querySelector(`${query}`).remove();
+}
